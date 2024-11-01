9ae42d167c93472389c6258ac4bfb1a1c0183cdb
Qayyum Ibrahim <temiyores1234@gmail.com>
2024-11-01 21:52:42 +0100

    fizzi completed

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,21 +3,22 @@ import { PrismicNextLink } from "@prismicio/next";
 import clsx from "clsx";
 
 type Props = {
-  buttonLink: LinkField;
+  buttonLink: string;
   buttonText: string | null;
   className?: string;
 };
 
 export default function Button({ buttonLink, buttonText, className }: Props) {
   return (
-    <PrismicNextLink
+    <a
       className={clsx(
         "rounded-xl bg-orange-600 px-5 py-4 text-center text-xl font-bold uppercase tracking-wide text-white transition-colors duration-150 hover:bg-orange-700 md:text-2xl",
         className,
       )}
-      field={buttonLink}
+      href={buttonLink}
+      target="_blank"
     >
       {buttonText}
-    </PrismicNextLink>
+    </a>
   );
 }
